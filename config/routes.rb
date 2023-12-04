1737478a07ccfb3ea54c2d59d3dcabf3c809865f
adogbeji <baka_1993@yahoo.co.uk>
2023-12-04 12:27:24 +0000

    Define name of route for specific articles

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   root 'articles#index'
   
   get '/articles', to: 'articles#index'
-  get '/articles/:id', to: 'articles#show'
+  get '/articles/:id', to: 'articles#show', as: :article
 end
