5f9c18d1508e53afa75bd7392457b1728ea0c986
adogbeji <baka_1993@yahoo.co.uk>
2023-12-09 11:15:34 +0000

    Add create action & route for handling form POST request

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,12 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+    
+    def create
+        @article = Article.new
+        
+        if @article.save
+            redirect_to articles_path
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   root 'articles#index'
   
   get '/articles', to: 'articles#index'
+  post '/articles', to: 'articles#create'
   get '/articles/new', to: 'articles#new'
   get '/articles/:id', to: 'articles#show', as: :article
   # get '/articles/new', to: 'articles#new'
