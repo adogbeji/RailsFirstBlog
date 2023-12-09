9cfc15677f3d4c53bb52d19e9ea6fdefebf5f45d
adogbeji <baka_1993@yahoo.co.uk>
2023-12-09 11:18:43 +0000

    Render article creation page if article is not saved

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,8 @@ class ArticlesController < ApplicationController
         
         if @article.save
             redirect_to articles_path
+        else
+            render :new
         end
     end
 end
